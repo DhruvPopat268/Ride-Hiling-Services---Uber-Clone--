c581dd874a6951daaa66a172a80d5cb3e38f6c02
Dhruv popat <dhruvpopat268@gmail.com>
2025-04-03 16:02:14 +0530

    user & captain login page done

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 const CaptainLogin = () => {
+
+  const [captaindata, setCaptaindata] = useState()
+
+  const { register, reset, handleSubmit, watch, formState: { errors, isSubmitting } } = useForm();
+
+  const onsubmit = (data) => {
+    setCaptaindata(data)
+  }
+
+  useEffect(() => {
+    console.log(captaindata)
+    reset()
+  }, [captaindata])
+
   return (
-    <div>CaptainLogin</div>
+    <>
+      <img className='w-20 m-5 ' src="https://imgs.search.brave.com/FZq7YFqzVbkjhipVXmxfaZY-RmPwy3wsG0WV1UdM8bs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/cy13b3JsZC5uZXQv/d3AtY29udGVudC91/cGxvYWRzLzIwMjAv/MDUvVWJlci1Mb2dv/LTcwMHgzOTQucG5n" alt='Uber_logo' />
+      <div className='m-10 '>
+        <form onSubmit={handleSubmit(onsubmit)}>
+
+          <h3 className='text-xl mb-2 font-bold'>What's your email</h3>
+
+          <input
+            className='bg-sky-100 rounded px-4 py-2 border w-full text-lg placeholder:text-base '
+            type='email'
+            {...register('email', { required: { value: true, message: "field is required" } })}
+            placeholder='enter your email'
+          />
+          {errors.email && <span className='text-red-500'>***{errors.email.message}</span>}
+
+          <h3 className='text-xl mb-2 mt-3 font-bold'>Enter password</h3>
+
+          <input
+            className='bg-sky-100 rounded px-4 py-2 border w-full text-lg placeholder:text-base  '
+            type='password'
+            {...register('password', { required: { value: true, message: "field is required" }, minLength: { value: 8, message: "8 chars are required" } })}
+            placeholder='enter your password'
+          />
+          {errors.password && <span className='text-red-500'>***{errors.password.message}</span>}
+
+          <div className=' h-80 mt-12 flex flex-col items-center justify-evenly'>
+            <button className='bg-black text-white font-bold rounded px-10 py-2'>Login</button>
+
+            <button className='bg-black text-white font-bold rounded px-10 py-2'>SignUp</button>
+            <p> New Here??
+              <Link to={'/UserLogin'} className=' text-blue-500 px-1 font-bold rounded  py-2'>Login as User</Link>
+            </p>
+
+          </div>
+
+        </form>
+      </div>
+
+    </>
   )
 }
 
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,20 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 const UserLogin = () => {
 
-  const [formData,setFormData] = useState()
+  const [userdata, setUserdata] = useState()
 
-  const { register, handleSubmit, watch, formState: { errors , isSubmitting } } = useForm();
+  const { register, reset, handleSubmit, watch, formState: { errors, isSubmitting } } = useForm();
 
-  const onsubmit = (data) =>{
-    setFormData(data)
-    console.log(formData)
+  const onsubmit = (data) => {
+    setUserdata(data)
   }
-  
+
+  useEffect(() => {
+    console.log(userdata)
+    reset()
+  }, [userdata])
+
   return (
-    <> 
+    <>
       <img className='w-20 m-5 ' src="https://imgs.search.brave.com/FZq7YFqzVbkjhipVXmxfaZY-RmPwy3wsG0WV1UdM8bs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/cy13b3JsZC5uZXQv/d3AtY29udGVudC91/cGxvYWRzLzIwMjAv/MDUvVWJlci1Mb2dv/LTcwMHgzOTQucG5n" alt='Uber_logo' />
       <div className='m-10 '>
         <form onSubmit={handleSubmit(onsubmit)}>
@@ -22,32 +26,36 @@ const UserLogin = () => {
           <h3 className='text-xl mb-2 font-bold'>What's your email</h3>
 
           <input
-            className='bg-sky-100 rounded px-4 py-2 border w-full text-lg placeholder:text-base mb-7  '
+            className='bg-sky-100 rounded px-4 py-2 border w-full text-lg placeholder:text-base '
             type='email'
-            {...register('email',{required:{value:true,message:"field is required"}})}
+            {...register('email', { required: { value: true, message: "field is required" } })}
             placeholder='enter your email'
           />
-         
-          <h3 className='text-xl mb-2 font-bold'>Enter password</h3>
+          {errors.email && <span className='text-red-500'>***{errors.email.message}</span>}
+
+          <h3 className='text-xl mb-2 mt-3 font-bold'>Enter password</h3>
 
           <input
-            className='bg-sky-100 rounded px-4 py-2 border w-full text-lg placeholder:text-base mb-7  '
+            className='bg-sky-100 rounded px-4 py-2 border w-full text-lg placeholder:text-base  '
             type='password'
-            {...register('password',{required:{value:true,message:"field is required"},minLength:{value:8,message:"8 chars are required"}})}
+            {...register('password', { required: { value: true, message: "field is required" }, minLength: { value: 8, message: "8 chars are required" } })}
             placeholder='enter your password'
           />
-          
-          <button type='submit' className='bg-black rounded font-medium mt-12   text-white px-10 py-2 ml-22'>Login</button>
+          {errors.password && <span className='text-red-500'>***{errors.password.message}</span>}
 
-          <p className=' ml-26 mt-6 font-bold text-red-500'>New Here ?</p>
+          <div className=' h-80 mt-12 flex flex-col items-center justify-evenly'>
+            <button className='bg-black text-white font-bold rounded px-10 py-2'>Login</button>
 
-          <button className='bg-black rounded font-medium mt-1   text-white px-10 py-2 ml-20'>Register</button>
-          
+            <button className='bg-black text-white font-bold rounded px-10 py-2'>SignUp</button>
+            <p> New Here??
+              <Link to={'/CaptainLogin'} className=' text-blue-500 px-1 font-bold rounded  py-2'>Login as Captain</Link>
+            </p>
+
+          </div>
+
         </form>
       </div>
-      <div className='text-center mt-15'>
-        <button className='bg-amber-200 rounded px-12 py-2 font-bold'>Login as Captain</button>
-      </div>
+
 
     </>
   )
